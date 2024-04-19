796cbe765fdc43be8c0d7aba792bf7ea2785a1fb
renanmarx26 <101154828+renanmarx26@users.noreply.github.com>
2024-04-19 04:11:41 -0300

    Produto.java

---
+++ Produto.java
@@ -0,0 +1,12 @@
+public class Produto {
+    double unicoProduto;
+    public Produto(double unicoProduto){
+        this.unicoProduto = unicoProduto;
+    }
+    public double calcularPrecoTotal(){
+        return unicoProduto;
+    }
+    public double calcularPrecoTotal(int quantidade){
+        return unicoProduto = unicoProduto;
+    }
+}
